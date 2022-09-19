c6285d9a556b1ea73b7652336054e37775c0f98e
KeyOne <trantuanduy.05112001@gmail.com>
2022-09-19 14:51:06 +0700

    update 6

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'products_grid.dart';
 import '../shared/app_drawer.dart';
 import '../cart/cart_screen.dart';
+import '../cart/cart_manager.dart';
+import 'top_right_badge.dart';
 enum FilterOptions { favorites, all }
 
 class ProductsOverviewScreen extends StatefulWidget {
@@ -30,13 +32,14 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   }
 
   Widget buildShoppingCarIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(Icons.shopping_cart),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: () {
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
     );
   }
 
